b9dae8ff6a819784116e5b9d1eb1e39d83135e27
Mir Sahib <mirsahib24@gmail.com>
2023-08-29 11:01:41 +0600

    :construction: working on swiper js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,102 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
-import "swiper/css";
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            "swiper-container": React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement>,
+                HTMLElement
+            >;
+            "swiper-slide": React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement>,
+                HTMLElement
+            >;
+        }
+    }
+}
 
-export default function App() {
+import { useRef, useEffect } from "react";
+import { register } from "swiper/element/bundle";
+//import { Navigation, Pagination } from "swiper/modules";
+register();
+
+const App = () => {
+    const swiperElRef = useRef(null);
+
+    useEffect(() => {
+        // listen for Swiper events using addEventListener
+        //@ts-ignore
+        swiperElRef.current.addEventListener("progress", (e) => {
+            const [swiper, progress] = e.detail;
+            console.log(progress);
+        });
+        //@ts-ignore
+        swiperElRef.current.addEventListener("slidechange", (e) => {
+            console.log("slide changed");
+        });
+    }, []);
+
     return (
-        <Swiper
-            spaceBetween={10}
-            slidesPerView={3}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
+        <swiper-container
+            ref={swiperElRef}
+            slides-per-view="3"
+            //@ts-ignore
+            navigation="true"
+            pagination="true"
+            spaceBetween={"50"}
+            
         >
-            <SwiperSlide>
-                <img src="https://dummyimage.com/600x400/000/58b8ba" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://dummyimage.com/600x400/000/c0c4c4" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://dummyimage.com/600x400/000/dbff3d" alt="" />
-            </SwiperSlide>
-        </Swiper>
+            <swiper-slide>
+                <div className="w-full h-screen flex items-center justify-center">
+                    <img
+                        className="h-full w-max-full object-cover"
+                        src="https://picsum.photos/900"
+                    />
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div className="w-full h-screen flex items-center justify-center">
+                    <img
+                        className="h-full w-max-full object-cover"
+                        src="https://picsum.photos/901"
+                    />
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div className="w-full h-screen flex items-center justify-center">
+                    <img
+                        className="h-full w-max-full object-cover"
+                        src="https://picsum.photos/902"
+                    />
+                </div>
+            </swiper-slide>
+        </swiper-container>
     );
+};
+
+export default App;
+
+{
+    /* <swiper-container
+ref={swiperElRef}
+
+slides-per-view="3"
+//@ts-ignore
+navigation="true"
+pagination="true"
+spaceBetween={50}
+>
+<swiper-slide>
+    <div className="w-full h-screen flex items-center justify-center">
+                <img
+                    className="h-full w-max-full object-cover"
+                    src="https://picsum.photos/900"
+                />
+            </div>
+</swiper-slide>
+<swiper-slide>
+    <img className="h-full w-max-full object-cover" src="https://picsum.photos/601" />
+</swiper-slide>
+<swiper-slide>
+    <img className="h-full w-max-full object-cover" src="https://picsum.photos/602" />
+</swiper-slide>
+</swiper-container> */
 }
