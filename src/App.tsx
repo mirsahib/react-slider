bee0ef94489e5a38fcd9b8266c50342ba3de9755
Mir Sahib <mirsahib24@gmail.com>
2023-08-29 16:56:01 +0600

    :hammer:  Add react-responsive carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,49 @@
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            "swiper-container": React.DetailedHTMLProps<
-                React.HTMLAttributes<HTMLElement>,
-                HTMLElement
-            >;
-            "swiper-slide": React.DetailedHTMLProps<
-                React.HTMLAttributes<HTMLElement>,
-                HTMLElement
-            >;
-        }
-    }
-}
-
-import { useRef, useEffect } from "react";
-import { register } from "swiper/element/bundle";
-//import { Navigation, Pagination } from "swiper/modules";
-register();
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const App = () => {
-    const swiperElRef = useRef(null);
-
-    useEffect(() => {
-        // listen for Swiper events using addEventListener
-        //@ts-ignore
-        swiperElRef.current.addEventListener("progress", (e) => {
-            const [swiper, progress] = e.detail;
-            console.log(progress);
-        });
-        //@ts-ignore
-        swiperElRef.current.addEventListener("slidechange", (e) => {
-            console.log("slide changed");
-        });
-    }, []);
-
     return (
-        <swiper-container
-            ref={swiperElRef}
-            slides-per-view="3"
-            //@ts-ignore
-            navigation="true"
-            pagination="true"
-            spaceBetween={"50"}
-            
+        <Carousel
+            //autoPlay={true}
+            //infiniteLoop={true}
+            showThumbs={false}
+            showIndicators={true}
+            showStatus={false}
         >
-            <swiper-slide>
-                <div className="w-full h-screen flex items-center justify-center">
-                    <img
-                        className="h-full w-max-full object-cover"
-                        src="https://picsum.photos/900"
-                    />
+            <div className="w-full h-[500px] flex items-center justify-center">
+                <img
+                    className="h-full max-w-full object-cover"
+                    src="https://picsum.photos/600"
+                />
+                <div className="absolute left-20 top-20 p-4 text-left w-1/3 text-white">
+                    <h4 className="mb-4 px-4 py-2 rounded-full bg-blue-500 text-white inline-block max-w-max">
+                        Perfomatech
+                    </h4>
+                    <p className="mb-2 text-3xl font-normal">
+                        Research and Innovation
+                    </p>
+                    <p className="mb-8 text-lg lg:text-2xl">
+                        Company culture oriented towards research and innovation
+                    </p>
+                    <button className="px-2 py-2 lg:w-7/12 rounded-sm bg-blue-500">About</button>
                 </div>
-            </swiper-slide>
-            <swiper-slide>
-                <div className="w-full h-screen flex items-center justify-center">
-                    <img
-                        className="h-full w-max-full object-cover"
-                        src="https://picsum.photos/901"
-                    />
-                </div>
-            </swiper-slide>
-            <swiper-slide>
-                <div className="w-full h-screen flex items-center justify-center">
-                    <img
-                        className="h-full w-max-full object-cover"
-                        src="https://picsum.photos/902"
-                    />
-                </div>
-            </swiper-slide>
-        </swiper-container>
+            </div>
+            <div className="w-full h-[500px] flex items-center justify-center">
+                <img
+                    className="h-full max-w-full object-cover"
+                    src="https://picsum.photos/601"
+                />
+                {/* <p className="legend">Legend 1</p> */}
+            </div>
+            <div className="w-full h-[500px] flex items-center justify-center">
+                <img
+                    className="h-full max-w-full object-cover"
+                    src="https://picsum.photos/602"
+                />
+                {/* <p className="legend">Legend 1</p> */}
+            </div>
+        </Carousel>
     );
 };
 
 export default App;
-
-{
-    /* <swiper-container
-ref={swiperElRef}
-
-slides-per-view="3"
-//@ts-ignore
-navigation="true"
-pagination="true"
-spaceBetween={50}
->
-<swiper-slide>
-    <div className="w-full h-screen flex items-center justify-center">
-                <img
-                    className="h-full w-max-full object-cover"
-                    src="https://picsum.photos/900"
-                />
-            </div>
-</swiper-slide>
-<swiper-slide>
-    <img className="h-full w-max-full object-cover" src="https://picsum.photos/601" />
-</swiper-slide>
-<swiper-slide>
-    <img className="h-full w-max-full object-cover" src="https://picsum.photos/602" />
-</swiper-slide>
-</swiper-container> */
-}
